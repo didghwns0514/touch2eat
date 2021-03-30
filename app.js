8ba1e24d5d2d9537ecc498a8a34b359e4b68f835
Hhhh Jjjj <didghwns0514@gmail.com>
2021-03-30 09:04:15 +0900

    configuring init

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 /*jslint node: true */
 "use strict";
 
+// wake up module
+const http = require("http");
+setInterval(function(){
+  http.get("http://touch2eat.herokuapp.com");
+}, 600000); // every 10
+
 const express = require("express");
 const PORT = process.env.PORT; // heroku port
 
